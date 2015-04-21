1f53c6270cade1e7f20312b0d3e27c68d7785899
Jean-Luc Desroches <jean18801880@gmail.com>
2015-04-21 10:13:43 -0400

    droppingg ionic

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,7 +32,6 @@ module.exports = function(config) {
       'app/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
       'app/bower_components/moment/moment.js',
       'app/bower_components/angular-flash/dist/angular-flash.js',
-      'app/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.js',
       'app/bower_components/angular-toggle-switch/angular-toggle-switch.js',
       'app/bower_components/ngCordova/dist/ng-cordova.js',
       'app/bower_components/gsap/src/uncompressed/TweenMax.js',
